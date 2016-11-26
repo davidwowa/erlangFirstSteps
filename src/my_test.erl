69e22c986e57ffcf1b2a52d3276747311ec73934
davidwowa <wd.zakrevskyy@icloud.com>
2016-11-26 20:20:47 +0100

    - merge

--- src/my_test.erl
+++ src/my_test.erl
@@ -12,6 +12,14 @@
 -export([create_positive_number_beam/1]).
 -export([create_negative_number_beam/1]).
 -export([create_number_beam/2]).
+%% ---
+-export([double/1]).
+-export([fac/1]).
+-export([tuppelExample/2]).
+-export([listExample/6]).
+-export([tail_recursive_fib/1]).
+-export([list_create/1]).
+-export([list_V/2]).
 
 %% ====================================================================
 %% Internal functions
@@ -31,3 +39,31 @@ create_negative_number_beam(0) -> [];
 create_negative_number_beam(N) when N < 0 -> create_negative_number_beam(N+1) -- [N].
 
 create_number_beam(F, T) when F < T -> create_number_beam(F, T) ++ [F+1].
+
+%% ------------------
+double(X) -> 2 * X.
+
+fac(1) -> 1;
+fac(N) ->
+N * fac(N - 1).
+
+tuppelExample(X, Y) ->
+	Point = {X, Y}.
+
+listExample(A, B, C, D, E, F) -> [A, B, C, D, E, F].
+
+tail_recursive_fib(N) ->
+    tail_recursive_fib(N, 0, 1, []).
+
+tail_recursive_fib(0, _Current, _Next, Fibs) ->
+    lists:reverse(Fibs);
+tail_recursive_fib(N, Current, Next, Fibs) -> 
+    tail_recursive_fib(N - 1, Next, Current + Next, [Current|Fibs]).
+
+list_create(0)->[];
+list_create(N) when N > 0 -> list_create(N-1) ++ [N].
+
+list_V(0, 0)->[];
+list_V(N, M) when N > 0, N < 101 -> add(M) ++ [M].
+
+add(N) -> N + N.
